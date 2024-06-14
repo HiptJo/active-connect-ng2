88a2b0d439d0afe4868a7ab2846d24fba704a16e
Jonas Hiptmair <office@hiptmairit.at>
2024-06-14 15:33:08 +0200

    v0.2.35 fixed export

--- projects/active-connect-ng2/src/lib/websocket/index.ts
+++ projects/active-connect-ng2/src/lib/websocket/index.ts
@@ -1,6 +1,7 @@
 export * from './client';
 export * from './decorators/websocket/handle';
 export * from './decorators/websocket/on-success';
+export * from './decorators/websocket/on-reconnect';
 export * from './decorators/websocket/outbound';
 export * from './decorators/websocket/route';
 export * from './decorators/websocket/shared';
